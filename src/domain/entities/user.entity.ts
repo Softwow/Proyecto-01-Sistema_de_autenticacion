d14e3c2f752be2305f50f787d13f11a6beca3a3d
carlos060798 <daniloangaritagarcia@gmail.com>
2023-12-07 11:49:41 -0500

    version 1.3 auth google

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -4,27 +4,27 @@
 export class UserEntity {
   constructor(
     public id: string,
-    public name: string,
-    public email: string,   
+    public nombre: string,
+    public  correo: string,   
     public password: string,
     public role: string,
   
   ) {}
 
   static fromObject(object: { [key: string]: any }) {
-    const { id, _id, name, email, password, role } = object;
+    const { id, _id, nombre, correo, password, role } = object;
 
     if (!id && !_id) throw Error("Id is required");
-    if (!name) throw Error("Name is required");
-    if (!email) throw Error("Email is required");
+    if (!nombre) throw Error("Name is required");
+    if (!correo) throw Error("Email is required");
 
     if (!password) throw Error("Password is required");
     if (!role) throw Error("Role is required");
 
     return new UserEntity(
       id || _id,
-      name,
-      email,
+      nombre,
+      correo,
       password,
       role
     );
